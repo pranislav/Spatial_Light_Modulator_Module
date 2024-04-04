d7a2fa016227207a97638b7a3f49b970dade8a3f
prano_windows <484757@mail.muni.cz>
2024-04-04 23:19:05 +0200

    make distances_and_lenses.py more flexible
    one global parameter into functions

--- lc-slm/src/distances_and_lenses.py
+++ lc-slm/src/distances_and_lenses.py
@@ -11,7 +11,6 @@ import constants as c
 # fixed stuff in mm
 small_dim_SLM = 28
 large_dim_SLM = 38
-f_out = 100
 f_mic = 9
 small_dim_CCD = 5
 large_dim_CCD = 6
@@ -21,21 +20,21 @@ diameter_mic = 9
 def distance1(f_in):
     return f_in * c.first_diff_max
 
-def distance2(f_in):
+def distance2(f_in, f_out):
     return distance1(f_in) * f_mic / f_out
 
-def f_cam(f_in):
-    return ('less_than', small_dim_CCD / distance2(f_in, f_out) * f_out)
+def f_cam(f_in, f_out):
+    return ('less_than', small_dim_CCD / distance1(f_in) * f_out)
 
 def telescope_magnification(f1, f2):
     return f2 / f1
 
-def width3(width1, f_in):
+def width3(width1, f_in, f_out):
     width2 = min(width1, large_dim_SLM)
     return telescope_magnification(f_in, f_out) * width2
 
-def is_efficient_mic(width1, f_in):
-    return width3(width1, f_in) < diameter_mic
+def is_efficient_mic(width1, f_in, f_out):
+    return width3(width1, f_in, f_out) < diameter_mic
 
 def is_efficient_SLM(width1):
     return width1 < small_dim_SLM
