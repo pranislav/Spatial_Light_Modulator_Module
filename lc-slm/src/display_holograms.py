18a51403c50c65b0b57c1499623864a7d27670ce
Branislav Palúch <x484757@fi.muni.cz>
2024-04-17 10:58:56 +0200

    update display_holograms.py - not tested yet

--- lc-slm/src/display_holograms.py
+++ lc-slm/src/display_holograms.py
@@ -7,31 +7,60 @@ import os
 
 def display_holograms(args):
     window = cl.create_tk_window()
-    directory = get_path(args.directory)
-    mask_name = args.mask_name
-    if args.mask_name:
-        mask_im = im.open(f"lc-slm/holograms_for_calibration/calibration_phase_masks/{args.mask_name}")
-        mask_arr = np.array(mask_im)
+    directory = set_dir(args.directory)
+    mask_arr = set_mask(args.mask_name)
     while True:
-        name = input("display hologram by typing its name, change directory with cd, change mask with cm >> ")
-        if name == "q": break
+        name = input("do an action or type 'help' >>")
+        if name == "help":
+            print_help()
+            continue
+        if name == "q":
+            break
         if name[0:3] == "cd ":
-            new_dir = name[3:-1]
-            if not(os.path.isdir(new_dir)):
-                print("specified directory does not exist")
+            directory = set_dir(name[3:])
+            continue
+        if name[0:2] == "cm":
+            if len(name) == 2: 
+                mask_arr = None
                 continue
-            directory = new_dir
-            print(f"directory changed to {new_dir}")
+            mask_arr = set_mask(name[3:-1])
             continue
-        if name[0:3] == "cm ":
-            mask_name = name[3:-1]
-            continue
-        path = f"{directory}/{name}"
-        if mask_name:
+        path = set_path_to_hologram(directory, name)
+        if mask_arr:
             cl.display_image_on_external_screen_img(window, mask_hologram(path, mask_arr))
         else:
             cl.display_image_on_external_screen(window, path)
 
+def set_path_to_hologram(directory, name):
+    if not os.path.isfile(f"{directory}/{name}"):
+        print("Error: specified hologram does not exist.")
+        return
+    return f"{directory}/{name}"
+
+def print_help():
+    print(f"cd <directory> - change directory - {directory_help}")
+    print("<hologram_name> - display hologram in current directory")
+    print(f"cm <mask_name> - change mask - {mask_help}")
+    print("help - display this message")
+    print("q - quit")
+
+def set_mask(mask_name):
+    if mask_name is None or mask_name == "" or mask_name == "none":
+        return None
+    if not os.path.isfile(f"lc-slm/holograms_for_calibration/calibration_phase_masks/{mask_name}"):
+        print("Error: specified mask does not exist. setting mask to None")
+        mask_arr = None
+        return    
+    mask_im = im.open(f"lc-slm/holograms_for_calibration/calibration_phase_masks/{args.mask_name}")
+    mask_arr = np.array(mask_im)
+    return mask_arr
+
+def set_dir(directory):
+    if not(os.path.isdir(directory)):
+        print("specified directory does not exist. make sure you are in project root and path is correct.")
+        return
+    print(f"directory changed to {directory}")
+    return directory
 
 def mask_hologram(path, mask_arr):
     hologram_im = im.open(path).convert("L")
@@ -41,31 +70,34 @@ def mask_hologram(path, mask_arr):
     return masked_hologram_im
 
 
-def get_path(mode):
-    if mode == 'p':
-        return ''
-    elif mode == 'h':
-        return "lc-slm/holograms"
-    elif mode == 'c':
-        return "lc-slm/holograms_for_calibration"
-    elif mode == 'i':
-        return "lc-slm/images"
-    else:
-        print("Error: Invalid directory value. Use one of: h, c, i, p. Type \"display_holograms.py --help for help\"") # TODO
+# def get_path(mode):
+#     if mode == 'p':
+#         return ''
+#     elif mode == 'h':
+#         return "lc-slm/holograms"
+#     elif mode == 'c':
+#         return "lc-slm/holograms_for_calibration"
+#     elif mode == 'i':
+#         return "lc-slm/images"
+#     else:
+#         print("Error: Invalid directory value. Use one of: h, c, i, p. Type \"display_holograms.py --help for help\"") # TODO
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="displays selected images from specified directory. Images can be masked with mask on given path")
+    parser = argparse.ArgumentParser(description="displays selected images from specified directory. Images can be masked with mask of given name")
 
     # Adding help message for directory argument
-    directory_help = """specifies directory containig images to be displayed:
-                        'h' for lc-slm/holograms
-                        'c' for lc-slm/holograms_for_calibration
-                        'i' for lc-slm/images
-                        'p' if you wish to specify full path to displayed image each time"""
+    # directory_help = """specifies directory containig images to be displayed:
+    #                     'h' for lc-slm/holograms
+    #                     'c' for lc-slm/holograms_for_calibration
+    #                     'i' for lc-slm/images
+    #                     'p' if you wish to specify full path to displayed image each time"""
+    
+    mask_help = "leave blank or type 'none' for no mask. otherwise type name of the mask. it have to be in lc-slm/holograms_for_calibration/calibration_phase_mask"
+    directory_help = "path (from project root) to directory containing images to be displayed"
 
-    parser.add_argument('mask_name', nargs='?', default=None, type=str, help="just name of the mask. it have to be in holograms_for_calibration/calibration_phase_mask")
-    parser.add_argument('-d', '--directory', choices=['h', 'c', 'i', 'p'], default='h', help=directory_help)
+    parser.add_argument('mask_name', nargs='?', default=None, type=str, help=mask_help)
+    parser.add_argument('-d', '--directory', default="lc-slm/holograms/animals_and_stuff", type=str, help=directory_help)
 
     args = parser.parse_args()
     display_holograms(args)
