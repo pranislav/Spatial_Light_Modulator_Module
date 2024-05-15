8b5a5086dc146f383362bb76d780fc4fe6516e04
prano_windows <484757@mail.muni.cz>
2024-05-15 12:01:46 +0200

    make color)phase_relation_two_big.py more general

--- lc-slm/src/color_phase_relation_two_big.py
+++ lc-slm/src/color_phase_relation_two_big.py
@@ -20,27 +20,27 @@ import os
 
 
 
-def main(precision, runs, wait, floor):
-    fit_func = f.positive_cos_floor() if floor else f.positive_cos
+def main(args):
+    fit_func = f.positive_cos_floor() if args.floor else f.positive_cos
     cam = uc480.UC480Camera()
     window = cl.create_tk_window()
-    subdomain_size = int(np.sqrt(1 / 5) * c.slm_height)
-    sample_list = e.make_sample_holograms((1, 1), precision)
-    upper_left_corner = np.array((c.slm_width // 2 - subdomain_size, (c.slm_height - subdomain_size) // 2))
+    # subdomain_size = int(np.sqrt(1 / 5) * c.slm_height)
+    sample_list = e.make_sample_holograms((1, 1), args.precision, args.ct2pi)
+    upper_left_corner = np.array((c.slm_width // 2 - args.subdomain_size, (c.slm_height - args.subdomain_size) // 2))
     black_hologram = im.fromarray(np.zeros((c.slm_height, c.slm_width)))
-    reference = cl.add_subdomain(black_hologram, sample_list[0], upper_left_corner, subdomain_size)
-    hologram_set = make_hologram_set(reference, sample_list, upper_left_corner + (subdomain_size, 0), subdomain_size)
+    reference = cl.add_subdomain(black_hologram, sample_list[0], upper_left_corner, args.subdomain_size)
+    hologram_set = make_hologram_set(reference, sample_list, upper_left_corner + (args.subdomain_size, 0), args.subdomain_size)
     cl.set_exposure_wrt_reference_img(cam, window, (220, 240), hologram_set[0], 8)
     intensity_coords = cl.get_highest_intensity_coordinates_img(cam, window, hologram_set[0], 8)
     fit_params_dict = {param: [] for param in fit_func.__code__.co_varnames[1:]}
     time_name = time.strftime("%Y-%m-%d_%H-%M-%S")
-    for i in range(runs):
-        intensity_list = two_big_loop(precision, cam, window, hologram_set, intensity_coords)
+    for i in range(args.runs):
+        intensity_list = two_big_loop(args.precision, cam, window, hologram_set, intensity_coords)
         param_dict = f.fit_intensity_general(intensity_list, fit_func)
         cp.fill_fit_params_dict(fit_params_dict, param_dict)
         make_plot(intensity_list, fit_params_dict, i, time_name)
-        print(f"run {i + 1}/{runs}")
-        sleep(wait)
+        print(f"run {i + 1}/{args.runs}")
+        sleep(args.wait)
     avg_params, std = cp.average_fit_params(fit_params_dict)
     cp.params_printout(avg_params, std)
 
@@ -85,8 +85,10 @@ def make_hologram_set(reference, sample_list, coords, subdomain_size):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--precision", type=int, default=32, help="number of different phase values to be tested")
+    parser.add_argument("-ss", "--subdomain_size", type=int, default=64, help="subdomain size")
+    parser.add_argument('-ct2pi', '--correspond_to2pi', type=int, default=256, help="value of pixel corresponding to 2pi phase shift")
     parser.add_argument("-r", "--runs", type=int, default=8, help="number of runs to average the results")
     parser.add_argument("-f", "--floor", action="store_true", help="presume that minimal intensity is almost zero")
     parser.add_argument("-w", "--wait", type=float, default=0.5, help="time to wait between runs")
     args = parser.parse_args()
-    main(args.precision, args.runs, args.wait, args.floor)
+    main(args)
