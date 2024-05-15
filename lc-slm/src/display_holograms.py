ee25ce2905c66d4f0b601c5aeb834495f7ebc8d9
prano_windows <484757@mail.muni.cz>
2024-05-15 12:03:20 +0200

    add mode for displaying instnat calibration holograms to display_holograms.py

--- lc-slm/src/display_holograms.py
+++ lc-slm/src/display_holograms.py
@@ -3,6 +3,8 @@ from PIL import Image as im
 import numpy as np
 import argparse
 import os
+import explore_calibration as e
+import constants as c
 
 mask_dir = "lc-slm/holograms_for_calibration/calibration_phase_masks"
 default_hologram_dir = "lc-slm/holograms"
@@ -28,21 +30,62 @@ def display_holograms(args):
         if command[0:2] == "cm":
             if len(command) == 2: 
                 mask_arr = None
-                display_with_mask(window, name, directory, mask_arr)
+                display_with_mask(window, name, directory, mask_arr, args.ct2pi)
                 continue
             maybe_mask_arr = set_mask(command[3:])
             if maybe_mask_arr is not None: mask_arr = maybe_mask_arr
-            display_with_mask(window, name, directory, mask_arr)
+            display_with_mask(window, name, directory, mask_arr, args.ct2pi)
+            continue
+        if command[:2] == 'c':
+            print("in mode for displaying instant calibration holograms")
+            display_instant_calibration_holograms()
             continue
         name = command
-        display_with_mask(window, name, directory, mask_arr)
+        display_with_mask(window, name, directory, mask_arr, args.ct2pi)
 
-def display_with_mask(window, name, directory, mask_arr): 
+
+def display_instant_calibration_holograms():
+    params = e.default_params()
+    while True:
+        get_params(params)
+        sample_hologram = cl.decline(params["decline"], 0, params["correspond_to_2pi"])
+        hologram = im.fromarray(np.zeros((c.slm_height, c.slm_width)))
+        hologram = cl.add_subdomain(hologram, sample_hologram, params["reference_position"], params["subdomain_size"])
+        if params["phase_shift"] != 0:
+            sample_hologram = cl.decline(params["decline"], params["phase_shift"], params["correspond_to_2pi"])
+        hologram = cl.add_subdomain(hologram, sample_hologram, params["subdomain_position"], params["subdomain_size"])
+        cl.display_image_on_external_screen(hologram)
+        command = input("press enter to continue, type 'q' to quit this mode >> ")
+        if command == 'q':
+            print("leaving mode for displaying instant calibration holograms")
+            break
+
+
+def default_params():
+    params = {}
+    params["correspond_to_2pi"] = 256
+    params["subdomain_size"] = 32
+    params["reference_position"] = (15, 11)
+    params["subdomain_position"] = (14, 11)
+    params["phase_shift"] = 0
+    params["decline"] = (1, 1)
+    return params
+
+def get_params(params):
+    print("enter parameters for calibration")
+    print("press enter to leave current value")
+    for key in params.keys():
+        value = input(f"{key} (current: {params[key]}) >> ")
+        if value != '':
+            params[key] = value
+
+
+def display_with_mask(window, name, directory, mask_arr, ct2pi): 
     path = set_path_to_hologram(directory, name)
     if path is None:
         return 
     if mask_arr is not None:
-        cl.display_image_on_external_screen_img(window, mask_hologram(path, mask_arr))
+        cl.display_image_on_external_screen_img(window, mask_hologram(path, mask_arr, ct2pi))
     else:
         cl.display_image_on_external_screen(window, path)
 
@@ -78,41 +121,22 @@ def set_dir(directory):
     print(f"directory changed to {directory}")
     return directory
 
-def mask_hologram(path, mask_arr):
+def mask_hologram(path, mask_arr, ct2pi):
     hologram_im = im.open(path).convert("L")
     hologram_arr = np.array(hologram_im)
-    masked_hologram_arr = (hologram_arr + mask_arr) % 256
+    masked_hologram_arr = (hologram_arr + mask_arr) % ct2pi
     masked_hologram_im = im.fromarray(masked_hologram_arr).convert("RGB")
     return masked_hologram_im
-
-
-# def get_path(mode):
-#     if mode == 'p':
-#         return ''
-#     elif mode == 'h':
-#         return "lc-slm/holograms"
-#     elif mode == 'c':
-#         return "lc-slm/holograms_for_calibration"
-#     elif mode == 'i':
-#         return "lc-slm/images"
-#     else:
-#         print("Error: Invalid directory value. Use one of: h, c, i, p. Type \"display_holograms.py --help for help\"") # TODO
 
 
 if __name__ == "__main__":
     # TODO: it actually dont need parsing now
     parser = argparse.ArgumentParser(description="displays selected images from specified directory. Images can be masked with mask of given name")
 
-    # Adding help message for directory argument
-    # directory_help = """specifies directory containig images to be displayed:
-    #                     'h' for lc-slm/holograms
-    #                     'c' for lc-slm/holograms_for_calibration
-    #                     'i' for lc-slm/images
-    #                     'p' if you wish to specify full path to displayed image each time"""
-    
     mask_help = f"leave blank or type 'none' for no mask. otherwise type name of the mask. it have to be in {mask_dir}"
     directory_help = "path (from project root) to directory containing images to be displayed"
 
+    parser.add_argument('-ct2pi', '--correspond_to2pi', type=int, default=256, help="value of pixel corresponding to 2pi phase shift")
     parser.add_argument('mask_name', nargs='?', default=None, type=str, help=mask_help)
     parser.add_argument('-d', '--directory', default=default_hologram_dir, type=str, help=directory_help)
 
