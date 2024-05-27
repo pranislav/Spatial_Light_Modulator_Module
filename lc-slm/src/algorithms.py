95afb8f97ff93033604e2837f88c92a5a75e1d3f
prano_windows <484757@mail.muni.cz>
2024-05-27 19:27:49 +0200

    reduce arguments in main versions of the algorithms

--- lc-slm/src/algorithms.py
+++ lc-slm/src/algorithms.py
@@ -3,7 +3,6 @@ from scipy.fft import fft2, ifft2, fftshift, ifftshift
 import matplotlib.pyplot as plt
 from random import random
 import PIL.Image as im
-from generate_and_transform_hologram_lib import gif_struct
 from typing import List, Tuple
 from cmath import phase
 
@@ -14,44 +13,43 @@ def is_zero(a: np.array) -> List[Tuple[int, int]]:
     return [(i, j) for i in range(len(a)) for j in range(len(a[0])) if a[i][j] == 0]
 
 
-def GS(target: np.array, path_to_incomming_intensity: str, tolerance: float, max_loops: int,\
-       gif_info: gif_struct, plot_error: bool=False, correspond_to2pi: int=256) -> np.array:
+def GS(target, args) -> np.array:
     '''classical Gerchberg-Saxton algorithm
     produces input for SLM for creating 'target' image
     '''
 
-    incomming_intensity = np.array(im.open(path_to_incomming_intensity))
+    incomming_intensity = np.array(im.open(args.path_to_incomming_intensity))
     incomming_amplitude = np.sqrt(incomming_intensity)
     w, l = target.shape
     target_amplitude = np.sqrt(target)
     space_norm = w * l
-    error = tolerance + 1
+    norm = np.amax(target)
+    error = args.tolerance + 1
     error_evolution = []
-    do_gif = gif_info.type
-    skip_frames = gif_info.skip_frames
     n = 0
     A = ifft2(target_amplitude)
-    while error > tolerance and n < max_loops:
+    while error > args.tolerance and n < args.max_loops:
         B = incomming_amplitude * np.exp(1j * np.angle(A))
         C = fftshift(fft2(B))
         D = np.abs(target_amplitude) * np.exp(1j * np.angle(C))
         A = (ifft2(ifftshift(D)))
         expected_outcome = np.abs(C)**2
-        expected_outcome *= 255 / expected_outcome.max()
+        expected_outcome *= norm / expected_outcome.max()
         error = error_f(expected_outcome, target, space_norm)
         error_evolution.append(error)
-        if do_gif and n % skip_frames == 0:
-            if do_gif == 'h':
-                img = im.fromarray((np.angle(A) + np.pi) * correspond_to2pi / (2*np.pi))
-            if do_gif == 'i':
+        if args.gif and n % args.gif_skip == 0:
+            if args.gif_type == 'h':
+                img = im.fromarray((np.angle(A) + np.pi) * args.correspond_to2pi / (2*np.pi))
+            if args.gif_type == 'i':
                 img = im.fromarray(expected_outcome)
-            img.convert("RGB").save(f"{gif_info.source_address}/{n // skip_frames}.jpg")
+
+            img.convert("RGB").save(f"{args.gif_source_address}/{n // args.gif_skip}.png")
         n+=1
         if n % 10 == 0: print("-", end='')
     print()
-    printout(error, n, error_evolution, "asdf", plot_error)
-    phase_for_slm = (np.angle(A) + np.pi) * correspond_to2pi / (2*np.pi) # converts phase to color value, input for SLM
-    return phase_for_slm, expected_outcome, n
+    printout(error, n, error_evolution, "asdf", args.plot_error)
+    phase_for_slm = (np.angle(A) + np.pi) * args.correspond_to2pi / (2*np.pi) # converts phase to color value, input for SLM
+    return phase_for_slm, expected_outcome
 
 
 def GS_for_moving_traps(target: np.array, tolerance: float, max_loops: int, correspond_to2pi: int=256) -> np.array:
@@ -61,6 +59,7 @@ def GS_for_moving_traps(target: np.array, tolerance: float, max_loops: int, corr
     w, l = target.shape
     target_amplitude = np.sqrt(target)
     space_norm = w * l
+    norm = np.amax(target)
     error = tolerance + 1
     error_evolution = []
     n = 0
@@ -71,7 +70,7 @@ def GS_for_moving_traps(target: np.array, tolerance: float, max_loops: int, corr
         D = np.abs(target_amplitude) * np.exp(1j * np.angle(C))
         A = (ifft2(ifftshift(D)))
         expected_outcome = np.abs(C) ** 2
-        expected_outcome *= 255 / expected_outcome.max()
+        expected_outcome *= norm / expected_outcome.max()
         error = error_f(expected_outcome, target, space_norm)
         error_evolution.append(error)
         n+=1
@@ -111,11 +110,9 @@ def GS_for_moving_traps(target: np.array, tolerance: float, max_loops: int, corr
 #     return phase_for_slm, exp_tar_for_slm
 
 
-def GD(demanded_output: np.array, path_to_incomming_intensity: str, learning_rate: float, enhance_mask: np.array,\
-       mask_relevance: float, tolerance: float, max_loops: int, unsettle, gif_info: gif_struct, plot_error: bool=False,\
-       correspond_to2pi: int=256) -> Tuple[np.array, np.array, int]:
+def GD(demanded_output: np.array, args) -> Tuple[np.array, np.array, int]:
     
-    incomming_intensity = np.array(im.open(path_to_incomming_intensity))
+    incomming_intensity = np.array(im.open(args.path_to_incomming_intensity))
     incomming_amplitude = np.sqrt(incomming_intensity)
     w, l = demanded_output.shape
     space_norm = w * l
@@ -123,33 +120,31 @@ def GD(demanded_output: np.array, path_to_incomming_intensity: str, learning_rat
     error_evolution = []
     norm = np.amax(demanded_output)
     input = initial_input
-    error = tolerance + 1
-    skip_frames = gif_info.skip_frames
-    do_gif = gif_info.type
+    error = args.tolerance + 1
     i = 0
     print("computing hologram (one bar for 10 loops)", ' ')
-    while error > tolerance and i < max_loops:
+    while error > args.tolerance and i < args.max_loops:
         med_output = fft2(input/abs(input) * incomming_amplitude)
         output_unnormed = abs(med_output) **2
-        output = output_unnormed / np.amax(output_unnormed) * norm # toto prip. zapocitat do grad. zostupu
-        mask = 1 + mask_relevance * enhance_mask
+        output = output_unnormed * norm / np.amax(output_unnormed) # toto prip. zapocitat do grad. zostupu
+        mask = 1 + args.mask_relevance * demanded_output/255
         dEdF = ifft2(mask * med_output * (output - demanded_output)) * incomming_amplitude
         dEdX = np.array(list(map(dEdX_complex, dEdF, input)))
         input -= learning_rate * dEdX
         error = error_f(output, demanded_output, space_norm)
         error_evolution.append(error)
-        if do_gif and i % skip_frames == 0:
-            if do_gif == 'h':
-                img = im.fromarray(complex_to_real_phase(input/abs(input), correspond_to2pi))
-            if do_gif == 'i':
+        if args.gif and i % args.gif_skip == 0:
+            if args.gif_type == 'h':
+                img = im.fromarray(complex_to_real_phase(input/abs(input), args.correspond_to2pi))
+            if args.gif_type == 'i':
                 img = im.fromarray(output)
-            img.convert("RGB").save(f"{gif_info.source_address}/{i // skip_frames}.jpg")
+            img.convert("RGB").save(f"{args.gif_source_address}/{i // args.gif_skip}.png")
         i += 1
-        if unsettle and i % int(max_loops / unsettle) == 0:
+        if args.unsettle and i % int(args.max_loops / args.unsettle) == 0:
             learning_rate *= 2
         if i % 10 == 0: print("-", end='')
-    printout(error, i, error_evolution, f"learning_rate: {learning_rate}", plot_error)
-    phase_for_slm = complex_to_real_phase(input, correspond_to2pi)
+    printout(error, i, error_evolution, f"learning_rate: {learning_rate}", args.plot_error)
+    phase_for_slm = complex_to_real_phase(input, args.correspond_to2pi)
     exp_tar_for_slm = output
     return phase_for_slm, exp_tar_for_slm, i
 
