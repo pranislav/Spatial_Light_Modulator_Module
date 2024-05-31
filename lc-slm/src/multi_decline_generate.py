1ac9c56df7ca179f27f66f8ab088832776a977f0
prano_windows <484757@mail.muni.cz>
2024-05-31 12:35:06 +0200

    change params

--- lc-slm/src/multi_decline_generate.py
+++ lc-slm/src/multi_decline_generate.py
@@ -1,11 +1,13 @@
+#TODO: redo this
+
 import multi_decline_lib as md
 from constants import slm_height as h, slm_width as w
 
 object = md.dot
 obj_size_x = 5
 obj_size_y = 5
-coord_generator = md.grating
-note = "5432"
+coord_generator = md.user_defined
+note = "two_dots_quarterized"
 spacing_param_a = 2 # num of objects on x axis for grating or x coordinate for 1-object position
 spacing_param_b = 2 # num of objects on y axis for grating or y coordinate for 1-object position
 
@@ -19,7 +21,7 @@ else:
     name = f"multidecline_{coord_generator.__name__}_{spacing_params}_{object.__name__}_{obj_size_x}x{obj_size_y}"
 
 if ud:
-    coordinates = [(w//5, h//4), (w//3, h//2)]
+    coordinates = [(w//6, h//4), (2*w//6, h//4)]
 else:
     coordinates = coord_generator(spacing_param_a, spacing_param_b)
 
